c3b0c1a0c5c1164960c2aed346efd0e11217c1ec
Sonter0077 <sontersimon29@gmail.com>
2023-02-17 15:13:39 -0800

    Hello, world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always O (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle"):
+		return (O):
+}

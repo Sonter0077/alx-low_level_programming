89d3b8261ff3307744f2556ad1e420e9472b567f
Sonter0077 <sontersimon29@gmail.com>
2023-02-17 18:13:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Ac program  that prints a line to the standard error
+ * main - Ac program that prints a line to the standard error
  * Return: Always 1
  */
 int main(void)
